3e550464c2726292503bf05ebdb070c3d274db0e
anirudhkuttu <97231757+anirudhkuttu@users.noreply.github.com>
2026-03-31 00:54:59 +0530

    Initial commit

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
-document.getElementById("feedbackForm").addEventListener("submit", async function(e) {
+document.getElementById("feedbackForm").addEventListener("submit", function(e) {
   e.preventDefault();
-
-  const name = document.getElementById("name").value;
-  const message = document.getElementById("message").value;
-
-  const res = await fetch("/api/feedback", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({ name, message })
-  });
-
-  const data = await res.json();
-  document.getElementById("responseMsg").innerText = data.message;
-
-  document.getElementById("feedbackForm").reset();
+  alert("Form is working!");
 });
